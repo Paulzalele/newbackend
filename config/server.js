9bbd8167f40923cb9ef465d8c83a92ff21bc76b3
Fuatia Salele <fuatia.salele@gmail.com>
2021-11-10 23:58:30 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b28e239dd89172f7ef889db6b1b49a9b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1bc3b238-0e46-4f35-8257-36276be272cb'
+};
