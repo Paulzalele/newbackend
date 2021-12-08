c05ae0a287df7e90157c78b237aaea80cad1c031
Fuatia Salele <fuatia.salele@gmail.com>
2021-12-08 15:21:06 -0600

    New Updates

--- api/blog/models/blog.js
+++ api/blog/models/blog.js
@@ -1,8 +1,36 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
+const slugify = require('slugify');
 
-module.exports = {};
+// Checks for identicle title then creates unques slug
+const getUniqueSlug = async (title, num=0) => {
+  let input = `${title}`;
+  if (num > 0) {
+    input = `${title}-${num}`;
+  }
+  const slug = slugify(input, {
+    lower: true
+   });
+  const [blog] = await strapi.services.blog.find({
+    slug: slug
+  });
+  if (!blog){
+    return slug;
+  }
+  else {
+    return getUniqueSlug(title, num+1);
+  }
+}
+
+module.exports = {
+/**
+   * Triggered before user creation.
+   */
+
+lifecycles: {
+    beforeCreate: async (data) => {
+      data.slug = await getUniqueSlug(data.title);
+    },
+  },
+
+};
--- config/functions/cron.js
+++ config/functions/cron.js
@@ -18,4 +18,22 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
+'*/1 * * * *': async () => {
+    // fetch articles to publish
+    const draftBlogToPublish = await strapi.api.blog.services.blog.find({
+      status: 'draft', // preview returns both draft and published entries
+    //  published_null: true,      // so we add another condition here to filter entries that have not been published
+      published_lt: new Date(),
+    });
+
+    // update published_at of articles
+    await Promise.all(draftBlogToPublish.map(blog => {
+      return strapi.api.blog.services.blog.update(
+        { id: blog.id },
+        { status: 'published'},
+        // { published_at: new Date() }
+      );
+    }));
+  },
+  
 };
